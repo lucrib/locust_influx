f71bc48c8cb05d51db870b75eb4245999b1424b7
Lucas Ribeiro <lribeiro@xanadu.ie>
2019-08-26 09:59:26 +0100

    Add ability to override measurement  and report request was successful or not

--- locust_influx/__init__.py
+++ locust_influx/__init__.py
@@ -3,37 +3,38 @@ from datetime import datetime
 from influxdb import InfluxDBClient
 from locust import events, runners
 
-influx_client = None
-
 
 def make_record(measurement, tags, fields, time):
     return [{"measurement": measurement, "tags": tags, "time": time, "fields": fields}]
 
 
-def ingest_data(influxdb_client, node_id):
+def ingest_data(influxdb_client, node_id, measurement, success):
     def save_to_influxdb(request_type=None, name=None, response_time=None, response_length=None, exception=None,
                          **kwargs):
         time = datetime.utcnow()
         tags = {
-            'node_id': node_id
-        }
-        fields = {
+            'node_id': node_id,
             'request_type': request_type,
             'name': name,
-            'response_time': response_time,
-            'response_length': response_length,
+            'success': success,
             'exception': str(exception)
         }
-        success = influxdb_client.write_points(make_record('request', tags, fields, time))
-        if not success:
+        fields = {
+            'response_time': response_time,
+            'response_length': response_length
+        }
+        point = make_record(measurement, tags, fields, time)
+        was_successful = influxdb_client.write_points(point)
+        if not was_successful:
             print('Failed to save to InfluxDB')
 
     return save_to_influxdb
 
 
-def expose_metrics(influx_host='localhost', influx_port=8086, user='root', pwd='root', db='locust'):
-    influx_client = InfluxDBClient(influx_host, influx_port, user, pwd, db)
-    influx_client.create_database(db)
+def expose_metrics(influx_host='localhost', influx_port=8086, user='root', pwd='root', database='locust',
+                   measurement='locust_requests'):
+    influx_client = InfluxDBClient(influx_host, influx_port, user, pwd, database)
+    influx_client.create_database(database)
     node_id = 'local'
     if runners.locust_runner:
         if runners.locust_runner.options.master:
@@ -41,5 +42,5 @@ def expose_metrics(influx_host='localhost', influx_port=8086, user='root', pwd='
         if runners.locust_runner.options.slave:
             node_id = runners.locust_runner.client_id
 
-    events.request_success += ingest_data(influx_client, node_id)
-    events.request_failure += ingest_data(influx_client, node_id)
+    events.request_success += ingest_data(influx_client, node_id, measurement, success=True)
+    events.request_failure += ingest_data(influx_client, node_id, measurement, success=False)
