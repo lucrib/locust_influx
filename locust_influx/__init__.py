07a85d1a977df2c10d0a7c0e5497919e4c7b2867
Lucas Ribeiro <lucasribeiro1990@gmail.com>
2019-09-21 11:53:42 +0100

    Make library code easier to read making use of globals

--- locust_influx/__init__.py
+++ locust_influx/__init__.py
@@ -10,19 +10,8 @@ __all__ = ['expose_metrics']
 
 log = logging.getLogger('locust_influx')
 
-
-class BooleanFlag:
-    def __init__(self, init_value: bool):
-        self.__flag = init_value
-
-    def __bool__(self):
-        return self.__flag
-
-    def set(self, new):
-        self.__flag = new
-
-    def get(self):
-        return self.__flag
+cache = []
+stop_flag = False
 
 
 def __make_data_point(measurement: str, tags: dict, fields: dict, time: datetime):
@@ -37,7 +26,7 @@ def __make_data_point(measurement: str, tags: dict, fields: dict, time: datetime
     return {"measurement": measurement, "tags": tags, "time": time, "fields": fields}
 
 
-def __listen_for_locust_events(cache: list, node_id: str, event: str):
+def __listen_for_locust_events(node_id: str, event: str):
     """
     Persist locust event such as hatching started or stopped to influxdb.
 
@@ -59,7 +48,7 @@ def __listen_for_locust_events(cache: list, node_id: str, event: str):
     return event_handler
 
 
-def __listen_for_requests_events(cache, node_id, success, measurement: str = 'locust_requests'):
+def __listen_for_requests_events(node_id, success, measurement: str = 'locust_requests'):
     """
     Persis request information to influxdb.
 
@@ -88,17 +77,17 @@ def __listen_for_requests_events(cache, node_id, success, measurement: str = 'lo
     return event_handler
 
 
-def __flush_points(influxdb_client: InfluxDBClient, cache: list):
+def __flush_points(influxdb_client: InfluxDBClient) -> None:
     """
     Write the cached data points to influxdb
 
     :param influxdb_client: An instance of InfluxDBClient
-    :param cache: The list holding the cached points to be flushed
-    :return:
+    :return: None
     """
+    global cache
     log.debug(f'Flushing points {len(cache)}')
-    to_be_flushed = cache.copy()
-    cache.clear()
+    to_be_flushed = cache
+    cache = []
     success = influxdb_client.write_points(to_be_flushed)
     if not success:
         log.error('Failed to write points to influxdb.')
@@ -106,10 +95,18 @@ def __flush_points(influxdb_client: InfluxDBClient, cache: list):
         cache.insert(0, to_be_flushed)
 
 
-def __flush_cached_points_worker(influxdb_client, interval, cache, stop_flag: BooleanFlag):
+def __flush_cached_points_worker(influxdb_client, interval) -> None:
+    """
+    Background job that puts the points into the cache to be flushed according tot he interval defined.
+
+    :param influxdb_client:
+    :param interval:
+    :return: None
+    """
+    global stop_flag
     log.info('Flush worker started.')
-    while not stop_flag.get():
-        __flush_points(influxdb_client, cache)
+    while not stop_flag:
+        __flush_points(influxdb_client)
         gevent.sleep(interval / 1000)
 
 
@@ -118,7 +115,7 @@ def expose_metrics(influx_host: str = 'localhost',
                    user: str = 'root',
                    pwd: str = 'root',
                    database: str = 'locust',
-                   interval_ms: int = 1000):
+                   interval_ms: int = 1000) -> None:
     """
     Attach event handlers to locust EventHooks in order to persist information to influxdb.
 
@@ -137,24 +134,23 @@ def expose_metrics(influx_host: str = 'localhost',
     if '--slave' in sys.argv:
         # TODO: Get real ID of slaves form locust somehow
         node_id = 'slave'
-    cache = []
-    finished = BooleanFlag(False)
-    # Start a greenlet that will save the data to influx according to the interval
-    flush_worker = gevent.spawn(__flush_cached_points_worker, influxdb_client, interval_ms, cache, finished)
+    # Start a greenlet that will save the data to influx according to the interval informed
+    flush_worker = gevent.spawn(__flush_cached_points_worker, influxdb_client, interval_ms)
     # Request events
-    events.request_success += __listen_for_requests_events(cache, node_id, success=True)
-    events.request_failure += __listen_for_requests_events(cache, node_id, success=False)
+    events.request_success += __listen_for_requests_events(node_id, success=True)
+    events.request_failure += __listen_for_requests_events(node_id, success=False)
     # Locust events
-    events.hatch_complete += __listen_for_locust_events(cache, node_id, event='hatch_complete')
-    events.quitting += __listen_for_locust_events(cache, node_id, event='quitting')
-    events.master_start_hatching += __listen_for_locust_events(cache, node_id, event='master_start_hatching')
-    events.master_stop_hatching += __listen_for_locust_events(cache, node_id, event='master_stop_hatching')
-    events.locust_start_hatching += __listen_for_locust_events(cache, node_id, event='locust_start_hatching')
-    events.locust_stop_hatching += __listen_for_locust_events(cache, node_id, event='locust_stop_hatching')
+    events.hatch_complete += __listen_for_locust_events(node_id, event='hatch_complete')
+    events.quitting += __listen_for_locust_events(node_id, event='quitting')
+    events.master_start_hatching += __listen_for_locust_events(node_id, event='master_start_hatching')
+    events.master_stop_hatching += __listen_for_locust_events(node_id, event='master_stop_hatching')
+    events.locust_start_hatching += __listen_for_locust_events(node_id, event='locust_start_hatching')
+    events.locust_stop_hatching += __listen_for_locust_events(node_id, event='locust_stop_hatching')
 
     def last_flush_on_quitting():
-        finished.set(True)
+        global stop_flag
+        stop_flag = True
         flush_worker.join()
-        __flush_points(influxdb_client, cache)
+        __flush_points(influxdb_client)
     # Flush last points when quiting
-    events.quitting += last_flush_on_quitting()
+    events.quitting += last_flush_on_quitting
